9b679e17aab469594ae1d50b660d0051b2c6787c
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-06-13 14:21:26 +0200

    update files

--- js/script.js
+++ js/script.js
@@ -1,61 +1,60 @@
 function printMessage(msg){
-    
-	var div = document.createElement('div');
-	div.innerHTML = msg; 
-	document.getElementById('messages').appendChild(div);
+  
+var div = document.createElement('div');
+div.innerHTML = msg; 
+document.getElementById('messages').appendChild(div);
 
 }
 
 function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-  var computerMove;
-  computerMove = 'kamień';
-  }
-  
-  printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-  
-  function clearMessages(){
-    document.getElementById('messages').innerHTML = '';
-    var computerMove;
-    computerMove = 'papier';
-    }
-    
-    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-    
+document.getElementById('messages').innerHTML = '';
+
+}
+
+//printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+
 
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
-  computerMove = 'kamień';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 } else if 
-    (randomnumber == '2') {
-        computerMove = 'nożyce';
-    } else if 
-    
-    (randomNumber == '3') {
-      computerMove = 'papier';
-    }
-    
-    printMessage('Mój ruch: ' + computerMove);
+  (randomNumber == '2') {
+      computerMove = 'nożyce';
+      printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+  } else if  
+  (randomNumber == '3') {
+    computerMove = 'papier';
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+  }
+  
+  printMessage('Mój ruch: ' + computerMove);
 
-    var playerMove, playerInput;
+  var playerMove, playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
 if (playerInput == '1') {
-  playerMove = 'kamień';
+playerMove = 'kamień';
+printMessage('Twój ruch: ' + playerMove);
 } else if 
 (playerInput == '2') {
-  playerMove = 'papier';
+playerMove = 'papier';
+printMessage('Twój ruch: ' + playerMove);
 } 
 else if 
 (playerInput == '3') {
-  playerMove = 'Nożyce';
+playerMove = 'Nożyce';
+printMessage('Twój ruch: ' + playerMove);
+}
+else {
+  printMessage('Wybierz poprawna odpowiedź');
 }
 
-printMessage('Twój ruch: ' + playerMove);
-     
-   
-
-
+//printMessage('Twój ruch: ' + playerMove);
+    
   
+
+
