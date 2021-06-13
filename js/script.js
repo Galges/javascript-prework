01868e83f2710bdab68195850c66c73355719e92
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-06-13 13:53:13 +0200

    add computer and playermove

---
+++ js/script.js
@@ -0,0 +1,61 @@
+function printMessage(msg){
+    
+	var div = document.createElement('div');
+	div.innerHTML = msg; 
+	document.getElementById('messages').appendChild(div);
+
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+  var computerMove;
+  computerMove = 'kamień';
+  }
+  
+  printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+  
+  function clearMessages(){
+    document.getElementById('messages').innerHTML = '';
+    var computerMove;
+    computerMove = 'papier';
+    }
+    
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+    
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if 
+    (randomnumber == '2') {
+        computerMove = 'nożyce';
+    } else if 
+    
+    (randomNumber == '3') {
+      computerMove = 'papier';
+    }
+    
+    printMessage('Mój ruch: ' + computerMove);
+
+    var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if 
+(playerInput == '2') {
+  playerMove = 'papier';
+} 
+else if 
+(playerInput == '3') {
+  playerMove = 'Nożyce';
+}
+
+printMessage('Twój ruch: ' + playerMove);
+     
+   
+
+
+  
